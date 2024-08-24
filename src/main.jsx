c6ed3c9c7017ef6622502aedaf141a4701d7749c
fujikihirotaka02 <fujikihirotaka02+pick@gmail.com>
2024-08-24 23:43:41 +0900

    jsx記法開始

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import {createRoot} from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const App = () => <h1>こんにちは！！</h1>
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+   <App /> 
+  </StrictMode>
+);
